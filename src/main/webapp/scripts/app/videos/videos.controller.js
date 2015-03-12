c780f4021f0ccf90502ee90a745cc24c9b1cac8c
kKadoc <kKadoc@hotmail.com>
2015-03-12 17:12:44 +0100

    ajout du switch d'affichage de la liste des vidéos

--- src/main/webapp/scripts/app/videos/videos.controller.js
+++ src/main/webapp/scripts/app/videos/videos.controller.js
@@ -1,13 +1,22 @@
 'use strict';
 
 angular.module('videothequeApp')
-    .controller('VideosController', function ($scope, Video) {
+    .controller('VideosController', function ($scope, localStorageService, Video) {
     	$scope.videos = [];
 
-    	$scope.loadAll = function() {
+    	$scope.refreshVideosList = function() {
     		Video.query(function(result) {
                $scope.videos = result;
             });
         };
-        $scope.loadAll();
+        $scope.refreshVideosList();
+        
+        $scope.display = localStorageService.get('display');
+        if ($scope.display == null) {
+        	$scope.display = "list";
+        }
+        $scope.changeDisplay = function(display) {
+        	$scope.display = display;
+        	localStorageService.set('display', display);
+        };
     });
