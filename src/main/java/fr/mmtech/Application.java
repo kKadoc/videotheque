a8dcb178b368bf05a89dd52b48bceda69ceef618
unknown <gtremouilles@ITEM-81418.dhcp.mtrl.fr.sopra>
2016-03-31 15:17:47 +0200

    Suppression de liquibase

--- src/main/java/fr/mmtech/Application.java
+++ src/main/java/fr/mmtech/Application.java
@@ -1,6 +1,13 @@
 package fr.mmtech;
 
-import fr.mmtech.config.Constants;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,17 +17,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
-import com.google.common.base.Joiner;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Arrays;
+import fr.mmtech.config.Constants;
 
 @ComponentScan
-@EnableAutoConfiguration(exclude = {MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = { MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class })
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
@@ -31,60 +32,59 @@ public class Application {
     /**
      * Initializes videotheque.
      * <p/>
-     * Spring profiles can be configured with a program arguments --spring.profiles.active=your-active-profile
+     * Spring profiles can be configured with a program arguments
+     * --spring.profiles.active=your-active-profile
      * <p/>
      */
     @PostConstruct
     public void initApplication() throws IOException {
-        if (env.getActiveProfiles().length == 0) {
-            log.warn("No Spring profile configured, running with default configuration");
-        } else {
-            log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
-        }
+	if (env.getActiveProfiles().length == 0) {
+	    log.warn("No Spring profile configured, running with default configuration");
+	} else {
+	    log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+	}
     }
 
     /**
      * Main method, used to run the application.
      */
     public static void main(String[] args) throws UnknownHostException {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setShowBanner(false);
+	SpringApplication app = new SpringApplication(Application.class);
+	app.setShowBanner(false);
 
-        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+	SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
 
-        // Check if the selected profile has been set as argument.
-        // if not the development profile will be added
-        addDefaultProfile(app, source);
-        addLiquibaseScanPackages();
-        Environment env = app.run(args).getEnvironment();
-        log.info("Access URLs:\n----------------------------------------------------------\n\t" +
-            "Local: \t\thttp://127.0.0.1:{}\n\t" +
-            "External: \thttp://{}:{}\n----------------------------------------------------------",
-            env.getProperty("server.port"),
-            InetAddress.getLocalHost().getHostAddress(),
-            env.getProperty("server.port"));
-
+	// Check if the selected profile has been set as argument.
+	// if not the development profile will be added
+	addDefaultProfile(app, source);
+	// addLiquibaseScanPackages();
+	Environment env = app.run(args).getEnvironment();
+	log.info("Access URLs:\n----------------------------------------------------------\n\t" + "Local: \t\thttp://127.0.0.1:{}\n\t"
+		+ "External: \thttp://{}:{}\n----------------------------------------------------------", env.getProperty("server.port"), InetAddress.getLocalHost().getHostAddress(),
+		env.getProperty("server.port"));
     }
 
     /**
      * Set a default profile if it has not been set
      */
     private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
-        if (!source.containsProperty("spring.profiles.active")) {
-            app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
-        }
+	if (!source.containsProperty("spring.profiles.active")) {
+	    app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
+	}
     }
 
-    /**
-     * Set the liquibases.scan.packages to avoid an exception from ServiceLocator.
-     */
-    private static void addLiquibaseScanPackages() {
-        System.setProperty("liquibase.scan.packages", Joiner.on(",").join(
-            "liquibase.change", "liquibase.database", "liquibase.parser",
-            "liquibase.precondition", "liquibase.datatype",
-            "liquibase.serializer", "liquibase.sqlgenerator", "liquibase.executor",
-            "liquibase.snapshot", "liquibase.logging", "liquibase.diff",
-            "liquibase.structure", "liquibase.structurecompare", "liquibase.lockservice",
-            "liquibase.ext", "liquibase.changelog"));
-    }
+    // /**
+    // * Set the liquibases.scan.packages to avoid an exception from
+    // ServiceLocator.
+    // */
+    // private static void addLiquibaseScanPackages() {
+    // System.setProperty("liquibase.scan.packages", Joiner.on(",").join(
+    // "liquibase.change", "liquibase.database", "liquibase.parser",
+    // "liquibase.precondition", "liquibase.datatype",
+    // "liquibase.serializer", "liquibase.sqlgenerator", "liquibase.executor",
+    // "liquibase.snapshot", "liquibase.logging", "liquibase.diff",
+    // "liquibase.structure", "liquibase.structurecompare",
+    // "liquibase.lockservice",
+    // "liquibase.ext", "liquibase.changelog"));
+    // }
 }
